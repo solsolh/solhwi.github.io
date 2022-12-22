ba7e77dbfa3afbd57fa1ac7dcb7dc914d753743e
solsolh <gnlthf21@gmail.com>
2022-12-22 23:50:44 +0900

    포르폴리오 용도변경 수정

--- js/script.js
+++ js/script.js
@@ -95,8 +95,25 @@ function typing(){
 };
 
 
-//swiper js
+//swiper js - work
 var swiper = new Swiper(".pro-slide", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//swiper js - coding
+var swiper = new Swiper(".pro-slide-2", {
     slidesPerView: 3,
     spaceBetween: 40,
     slidesPerGroup: 3,
